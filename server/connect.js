3bbe6fc187f65c89f32aa640855808525aa5206b
csm-GH <sinmanchau25@gmail.com>
2025-05-01 18:58:16 +0800

    Update connect.js
    update

--- server/connect.js
+++ server/connect.js
@@ -1,7 +1,5 @@
 const mysql = require('mysql2');
 
-// i download a SQL database in my computer and i use it to test the server.js so the host is localhost
-
 // Create MySQL connection
 const connection = mysql.createConnection({
   host: 'localhost',
